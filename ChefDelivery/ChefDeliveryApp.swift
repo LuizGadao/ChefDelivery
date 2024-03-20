c0008a025975a6a02bf59e16147410522d2b941d
Luiz <luizgadao@gmail.com>
2024-03-20 18:57:07 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Luiz Carlos Goncalves Dos Anjos on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
