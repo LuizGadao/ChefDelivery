3d67bdbd4ef28c38b77f4c0113108fc94e0d70d1
Luiz <luizgadao@gmail.com>
2024-03-25 20:11:20 -0300

    create layout for DetailsView

--- ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/StoresView/StoreDetailView.swift
@@ -12,7 +12,66 @@ struct StoreDetailView: View {
     let store: StoreType
     
     var body: some View {
-        Text("Hello, \(store.name)")
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading) {
+                Image(store.headerImage)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack{
+                    Text(store.name)
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Image(store.logoImage)
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                
+                HStack {
+                    Text(store.location)
+                    
+                    ForEach(1...store.stars, id: \.self){ _ in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                            .font(.caption)
+                    }
+                }
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                
+                Text("Produtos")
+                    .font(.title2)
+                    .bold()
+                    .padding(.vertical, 8)
+                    .padding(.horizontal)
+                
+                ForEach(store.products) { product in
+                    HStack(spacing: 8) {
+                        VStack(alignment:.leading, spacing: 8){
+                            Text(product.name).bold()
+                            Text(product.description).foregroundColor(.black.opacity(0.5))
+                            Text("\(product.price)")
+                        }
+                        
+                        Spacer()
+                        
+                        Image(product.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(12)
+                            .frame(width: 120, height: 120)
+                            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 10, x: 6, y: 6)
+                    }
+                }
+                .padding()
+                
+            }
+            .navigationTitle(store.name)
+        .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
